ba7666a253aed61fb1c111c529f92740ce268ed3
Hedi Temani <hedi.temani97@gmail.com>
2026-03-13 15:18:13 +0100

    Add profile view with GET and PUT methods for user data retrieval and update

--- users/views.py
+++ users/views.py
@@ -74,7 +74,21 @@ class LogoutView(APIView):
 
 #PROFILE
 
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
 
+    def get(self,request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+    
+
+    def put(self, request):
+
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
