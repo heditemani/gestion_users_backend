65b134e848ba34df4828d10550b5ecd41e0b3d6c
Hedi Temani <hedi.temani97@gmail.com>
2026-03-13 15:23:25 +0100

    Add URL routing for user authentication and profile management

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import RegisterView, LoginView, LogoutView, ProfileView
+
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register')
+    path('login/', LoginView.as_view(), name='login')
+    path('logout/', LoginView.as_view(), name='logout')
+    path('profile/', ProfileView.as_view(), name='profile')
+    path('token/refrsh/', TokenRefreshView.as_view(), name='token_refresh')
+]
