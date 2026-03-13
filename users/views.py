87026cee0001fb461c3b8dc1a553b93747055eab
Hedi Temani <hedi.temani97@gmail.com>
2026-03-13 15:13:10 +0100

    Implement login and logout functionality in views.py

--- users/views.py
+++ users/views.py
@@ -31,7 +31,53 @@ class RegisterView(APIView):
 
 
 class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self,request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+
+        user = authenticate(username = username, password = password)
+
+        if user : 
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                "message" : "Login réussi !",
+                "access"  : str(refresh.access_token),
+                "refresh" : str(refresh),
+                "user" : UserSerializer(user).data
+            })
+        
+        return Response({
+            "error" : "Username ou Password Incorrect" 
+        },status=status.HTTP_401_UNAUTHORIZED)
     
+
+
+#LOGOUT
+
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]  # ma ynajam yamalha ken eli aml login
+
+    def post(self, request):
+        try : 
+            RefreshToken = request.data.get('refresh')
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"message" : "Lougout réussi !"})
+        except :
+            return Response({"message" : "Token Incorrect !"},status=status.HTTP_400_BAD_REQUEST )
+        
+
+
+#PROFILE
+
+
+
+
+
 
 
 
